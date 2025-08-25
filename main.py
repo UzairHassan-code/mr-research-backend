01bb40ea91eaad7d4500df1d665f8d61ce004745
M.Uzair Hassan <cuzair40@gmail.com>
2025-08-25 13:09:06 +0500

    URL Added Correctly

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ research_graph = create_research_graph()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"
-                   "https://mr-research-frontend.vercel.app/"],
+                   "https://mr-research-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
